bd4db1bfbf09ca793f3c05c884c18d88acbd7e01
limielife <selim0401@gmail.com>
2024-12-13 21:14:11 +0900

    execute

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-class App {
-  async play() {}
+import { NumberBaseball } from "./NumberBaseball.js";
+
+export class App {
+    async play() {
+        const game = new NumberBaseball();
+        await game.play(); // 게임 시작
+    }
 }
 
 export default App;
